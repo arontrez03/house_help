f5b2bccbd54e0b925501b655bb3145cc67e26600
arontrez03 <arontrez03@gmail.com>
2020-03-09 22:31:08 +0800

    Create AppDelegate.h

---
+++ HouseHelp/HouseHelp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HouseHelp
+//
+//  Created by Breakstuff on 9/20/13.
+//  Copyright (c) 2013 FastData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HouseHelpMainViewController.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
